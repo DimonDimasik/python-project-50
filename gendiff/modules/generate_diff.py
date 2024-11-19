e485690d5648729c47ef211f256aadde16b6cbc7
Dmitrii <dimalazarevne@gmail.com>
2024-11-19 16:37:19 +0300

    add step 4

--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -1,8 +1,35 @@
 import json
 
 
+def assort(iter):
+    return sorted(iter, key=lambda item: item[1])
+
+
+def convert(iter):
+    for i in iter:
+        print(i[0] + i[1] + i[2])
+
+
+def collate(first, second):
+    compare_list = []
+    for item in first:
+        if item in second:
+            if first[item] == second[item]:
+                compare_list.append(('  ', f'{item}: '.lower(), f'{first[item]}'.lower()))
+            else:
+                compare_list.append(('- ', f'{item}: '.lower(), f'{first[item]}'.lower()))
+                compare_list.append(('+ ', f'{item}: '.lower(), f'{second[item]}'.lower()))
+        else:
+            compare_list.append(('- ', f'{item}: '.lower(), f'{first[item]}'.lower()))
+    for item in second:
+        if item not in first:
+            compare_list.append(('+ ', f'{item}: '.lower(), f'{second[item]}'.lower()))
+    return compare_list
+
+
 def generate_diff(first_file, second_file):
     first_file = json.load(open(first_file))
     second_file = json.load(open(second_file))
-    print(first_file)
-    print(second_file)
+    diff_list = collate(first_file, second_file)
+    diff_list = assort(diff_list)
+    convert(diff_list)
