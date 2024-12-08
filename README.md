7b17683c07fd132444b977fe3407aba5a7069723
Dmitrii <dimalazarevne@gmail.com>
2024-12-08 19:43:38 +0300

    add badges

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<a href="https://codeclimate.com/github/DimonDimasik/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/4f22b711b4f8a355fcf6/maintainability" /></a>
+
+<a href="https://codeclimate.com/github/DimonDimasik/python-project-50/test_coverage"><img src="https://api.codeclimate.com/v1/badges/4f22b711b4f8a355fcf6/test_coverage" /></a>
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DimonDimasik/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DimonDimasik/python-project-50/actions)
 
--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -6,8 +6,10 @@ def assort(iter):
 
 
 def convert(iter):
+    string = ''
     for i in iter:
-        print(i[0] + i[1] + i[2])
+        string += i[0] + i[1] + i[2]
+    return string[:-2]
 
 
 def collate(first, second):
@@ -15,15 +17,25 @@ def collate(first, second):
     for item in first:
         if item in second:
             if first[item] == second[item]:
-                compare_list.append(('  ', f'{item}: '.lower(), f'{first[item]}'.lower()))
+                compare_list.append(
+                    ('  ', f'{item}: '.lower(), f'{first[item]}\n'.lower())
+                )
             else:
-                compare_list.append(('- ', f'{item}: '.lower(), f'{first[item]}'.lower()))
-                compare_list.append(('+ ', f'{item}: '.lower(), f'{second[item]}'.lower()))
+                compare_list.append(
+                    ('- ', f'{item}: '.lower(), f'{first[item]}\n'.lower())
+                )
+                compare_list.append(
+                    ('+ ', f'{item}: '.lower(), f'{second[item]}\n'.lower())
+                )
         else:
-            compare_list.append(('- ', f'{item}: '.lower(), f'{first[item]}'.lower()))
+            compare_list.append(
+                ('- ', f'{item}: '.lower(), f'{first[item]}\n'.lower())
+            )
     for item in second:
         if item not in first:
-            compare_list.append(('+ ', f'{item}: '.lower(), f'{second[item]}'.lower()))
+            compare_list.append(
+                ('+ ', f'{item}: '.lower(), f'{second[item]}\n'.lower())
+            )
     return compare_list
 
 
@@ -32,4 +44,4 @@ def generate_diff(first_file, second_file):
     second_file = json.load(open(second_file))
     diff_list = collate(first_file, second_file)
     diff_list = assort(diff_list)
-    convert(diff_list)
+    print(convert(diff_list))
