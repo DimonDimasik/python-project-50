e40a71dee7d42d2baf8f832667c65cb14097f327
Dmitrii <dimalazarevne@gmail.com>
2025-01-27 21:01:47 +0300

    add test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,14 @@
-from gendiff.modules.generate_diff import assort
+from gendiff.modules.generate_diff import assort, convert
 
 
 def test_assort():
     assert assort(['cotlin', 'rust', 'dart']) == ['dart', 'cotlin', 'rust']
+    assert assort([['+', 'c', '-'],['z', 'a', 'b'], ['1', 'b', '9']]) == [['z', 'a', 'b'], ['1', 'b', '9'], ['+', 'c', '-']]
+
+
+def test_convert():
+    assert convert([['he', 'll', 'o, '], ['wo', 'rl', 'd!!!']]) == 'hello, world!'
+
+
+test_assort()
+test_convert()
