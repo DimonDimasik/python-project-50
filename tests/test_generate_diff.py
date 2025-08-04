12407f699cf3a3d9463d5e1630ebd10aa2858e7c
Dmitrii <dimalazarevne@gmail.com>
2025-08-04 23:12:07 +0300

    add tests fix

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -52,11 +52,6 @@ def test_empty_yaml_generate_diff(yaml_file1, yaml_empty):
     assert generate_diff(yaml_file1, yaml_empty) == expected
 
 
-def test_json_generate_diff_nested(json_file21, json_file22):
-    expected = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}'
-    assert generate_diff(json_file21, json_file22) == expected
-
-
 @pytest.fixture
 def json_file21():
     return 'tests/fixtures/file21.json'
@@ -65,3 +60,8 @@ def json_file21():
 @pytest.fixture
 def json_file22():
     return 'tests/fixtures/file22.json'
+
+
+def test_json_generate_diff_nested(json_file21, json_file22):
+    expected = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}'
+    assert generate_diff(json_file21, json_file22) == expected
