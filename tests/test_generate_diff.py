f0a5915689300662c693ae6d4541a4c7dcda34f2
Dmitrii <dimalazarevne@gmail.com>
2025-08-14 21:59:09 +0300

    add test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,5 +1,5 @@
 import pytest
-from gendiff.modules.generate_diff import generate_diff
+from gendiff.modules.generate_diff import generate_diff, format_value
 
 
 @pytest.fixture
@@ -65,3 +65,10 @@ def json_file22():
 def test_json_generate_diff_nested(json_file21, json_file22):
     expected = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}'
     assert generate_diff(json_file21, json_file22) == expected
+
+
+def test_format_value():
+    assert format_value(True) == 'true'
+    assert format_value(False) == 'false'
+    assert format_value(55) == '55'
+    
